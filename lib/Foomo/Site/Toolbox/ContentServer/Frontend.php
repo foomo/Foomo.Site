dffbdc2ae921fa6200df673bc03c9a7ba1122d6f
Jan Halfar <jan@bestbytes.com>
2016-11-29 15:56:13 +0100

    high memory limit for frontend

--- lib/Foomo/Site/Toolbox/ContentServer/Frontend.php
+++ lib/Foomo/Site/Toolbox/ContentServer/Frontend.php
@@ -38,4 +38,11 @@ class Frontend extends \Foomo\MVC\AbstractApp
 	 * @var Frontend\Controller
 	 */
 	public $controller;
+	public function __construct($appClassName)
+	{
+		ini_set("memory_limit", "2048M");
+		parent::__construct($appClassName);
+	}
+
 }
+
