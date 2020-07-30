4c7cd223c75dceeee176277e8e135ede372c72fc
Bostjan Marusic <bostjan@bestbytes.de>
2020-07-30 20:45:28 +0200

    reverted

--- htdocs/services/contentServer.php
+++ htdocs/services/contentServer.php
@@ -1,5 +1,4 @@
 <?php
-
 \Foomo\Services\RPC::create(new \Foomo\Site\Service\ContentServer())
 	->clientNamespace('Foomo.Services.ContentServer')
 	->serializeWith(new \Foomo\Services\RPC\Serializer\JSON())
